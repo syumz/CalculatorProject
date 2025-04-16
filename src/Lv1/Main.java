da8f5d3ff41760a50366c7c30ce920b1608347d5
seoyeon <dlatjdus0303@naver.com>
2025-04-16 13:13:08 +0900

    feat: '양의 정수 2개 입력받기' 추가

--- src/Lv1/Main.java
+++ src/Lv1/Main.java
@@ -1,7 +1,15 @@
 package Lv1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int firstNum = scanner.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int secondNum = scanner.nextInt();
     }
 }

ff567824c213c47551944007532860db90e67465
seoyeon <dlatjdus0303@naver.com>
2025-04-16 17:59:35 +0900

    chore: rename Main to App in Lv1

--- src/Lv1/App.java
+++ src/Lv1/App.java
@@ -2,7 +2,7 @@ package Lv1;
 
 import java.util.Scanner;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 

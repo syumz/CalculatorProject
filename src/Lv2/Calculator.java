33345c48bd34db4166880e25080678d37d367fce
seoyeon <dlatjdus0303@naver.com>
2025-04-17 09:17:09 +0900

    feat: Implement Calculator with arithmetic and a collection for result storage

--- src/Lv2/Calculator.java
+++ src/Lv2/Calculator.java
@@ -1,4 +1,29 @@
 package Lv2;
 
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Calculator {
+
+    // 1. 속성
+    ArrayList<Integer> resultSave = new ArrayList<>();
+
+    // 2. 생성자
+
+
+    // 3. 기능(메서드)
+    public int calculate(int num1, int num2, char operator) {
+        int result = 0;
+        if(operator=='+'){
+            result = num1 + num2;
+        } else if(operator == '-'){
+            result = num1 - num2;
+        } else if(operator == '*'){
+            result = num1 * num2;
+        } else if(operator == '/'){
+            result = num1 / num2;
+        }
+        return result;
+    }
 }

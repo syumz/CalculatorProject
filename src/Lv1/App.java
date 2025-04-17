027127b01c903e20927c54c5dfeba51075f4fd75
seoyeon <dlatjdus0303@naver.com>
2025-04-17 15:27:09 +0900

    docs: Add comments to clarify logic

--- src/Lv1/App.java
+++ src/Lv1/App.java
@@ -10,7 +10,8 @@ public class App {
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
 
-        for(;;) { // break;로 빠져나오기 전까지 계속 반복
+        // break;로 빠져나오기 전까지 계속 반복
+        for(;;) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = scanner.nextInt(); // 첫 번째 숫자 입력
 
@@ -21,16 +22,17 @@ public class App {
             char operator = scanner.next().charAt(0); // 사칙연산 기호 입력
 
 
-
+            // 두 정수 연산
             if (num2 == 0 && operator == '/') {
                 System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-
+                // 두 번째 숫자가 0 이고 사칙연산 기호가 / 일 때 계산할 수 없으므로 '입력 될 수 없다'을 출력
             } else {
                 int result = calculator.calculate(num1, num2, operator);
                 System.out.println("결과: " + result); // 연산 결과 출력
                 calculator.getResultSave().add(result);
             }
 
+            // 저장된 연산 결과 값을 보여주고 "yes"을 작성시 가장 먼저 저장된 값 삭제
             System.out.println("저장된 연산 결과 " + calculator.getResultSave());
             calculator.removeResult();
 
--- src/Lv2/Calculator.java
+++ src/Lv2/Calculator.java
@@ -6,11 +6,14 @@ import java.util.Scanner;
 public class Calculator {
 
     // 1. 속성
+    // 연산 결과를 저장하는 변수 생성
     private ArrayList<Integer> resultSave = new ArrayList<>();
 
     // 2. 생성자
+    // 기본 생성자
 
     // 3. 기능(메서드)
+    // 연산 기능 메서드
     public int calculate(int num1, int num2, char operator) {
         int result = 0;
         if(operator=='+'){
@@ -25,14 +28,17 @@ public class Calculator {
         return result;
     }
 
+    // 게터 메서드
     public ArrayList<Integer> getResultSave() {
         return resultSave;
     }
 
+    // 세터 메서드
     public void setResultSave(ArrayList<Integer> resultSave) {
         this.resultSave = resultSave;
     }
 
+    // "yes"을 작성하면 가장 먼저 저장된 값을 지우는 메서드
     public void removeResult(){
         System.out.print("가장 먼저 저장된 연산 결과를 지우시겠습니까? (yes 입력 시 삭제): ");
         Scanner scanner = new Scanner(System.in);

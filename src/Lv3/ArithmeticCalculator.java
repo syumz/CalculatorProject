29b133c5f1602356740053860cdcde9823e6ab1f
seoyeon <dlatjdus0303@naver.com>
2025-04-23 13:19:55 +0900

    refactor: Add generics to ArithmeticCalculator for type flexibility

--- src/Lv3/ArithmeticCalculator.java
+++ src/Lv3/ArithmeticCalculator.java
@@ -1,7 +1,29 @@
 package Lv3;
 
 public class ArithmeticCalculator <T>{
+    // 1. 속성
+    private T result;
 
+    // 2. 생성자
+
+    // 3. 기능
+    // 연산
+    public static <T extends Number> void sum(T num1, T num2, char operator) {
+        OperatorType sum = OperatorType.SUM;
+        System.out.println("결과 : " + sum.calculate(((Integer) num1).doubleValue(), ((Integer) num2).doubleValue()));
+    }
+    public static <T extends Number> void sub(T num1, T num2, char operator) {
+        OperatorType sub = OperatorType.SUB;
+        System.out.println("결과 : " + sub.calculate(((Integer) num1).doubleValue(), ((Integer) num2).doubleValue()));
+    }
+    public static <T extends Number> void div(T num1, T num2, char operator) {
+        OperatorType div = OperatorType.DIV;
+        System.out.println("결과 : " + div.calculate(((Integer) num1).doubleValue(), ((Integer) num2).doubleValue()));
+    }
+    public static <T extends Number> void mul(T num1, T num2, char operator) {
+        OperatorType mul = OperatorType.MUL;
+        System.out.println("결과 : " + mul.calculate(((Integer) num1).doubleValue(), ((Integer) num2).doubleValue()));
+    }
 
 
 }

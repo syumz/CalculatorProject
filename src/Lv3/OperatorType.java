36f0fef5f231d823ee9f0488b3499862b6050288
seoyeon <dlatjdus0303@naver.com>
2025-04-23 10:37:29 +0900

    refactor: Update OperatorType enum to include BiFunction for calculation

--- src/Lv3/OperatorType.java
+++ src/Lv3/OperatorType.java
@@ -1,18 +1,26 @@
 package Lv3;
 
+import java.util.function.BiFunction;
+
 public enum OperatorType {
-    SUM('+'),
-    SUB('-'),
-    DIV('/'),
-    MUL('*');
+    SUM('+', (num1, num2) -> num1 + num2),
+    SUB('-', (num1, num2) -> num1 - num2),
+    DIV('/', (num1, num2) -> num1 / num2),
+    MUL('*', (num1, num2) -> num1 * num2);
 
-    private char operator;
+    // 1. 속성
+    private final char operator;
+    private final BiFunction<Double, Double, Double> result;
 
-    OperatorType(char operator) {
+    // 2. 생성자
+    OperatorType(char operator, BiFunction<Double, Double, Double> result) {
         this.operator = operator;
+        this.result = result;
     }
 
-    public char getOperator() {
-        return operator;
+    // 3. 기능
+    public Double calculate(Double num1, Double num2){
+        return this.result.apply(num1, num2);
     }
-};
+
+}
